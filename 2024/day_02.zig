1d7bb2fca9823459c8eacbf953f9a1665c6992e5
Vadivel Kumar <vadivtk@gmail.com>
2024-12-08 23:14:55 +0530

    Initial setup code for day 2

---
+++ 2024/day_02.zig
@@ -0,0 +1,9 @@
+// Day 2: Red-nosed Reports
+//
+// Important facts:
+//
+// - Each report is a list of numbers called **levels** that are separated by spaces.
+// - The engineers are trying to figure out which reports are safe.
+// - The safety systems can only tolerate levels that are either gradually increasing or decreasing.
+// - A report only counts as SAFE if both of the folloing are true - a) levels are either all increasing or all decreasing b) Any two adjacent levels differ by at least one and at most three.
+//
